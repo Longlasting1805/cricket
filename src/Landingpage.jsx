cc4d24466339c704facdc0d741a3b0575e964896
Akande Al-ameen <longlasting1805@gmail.com>
2023-09-19 11:03:16 +0100

    navbar

--- src/Landingpage.jsx
+++ src/Landingpage.jsx
@@ -7,7 +7,19 @@ export const Landingpage = (props) =>{
         <div className="root-div-container">
             <div className="cricket-logo">
                 <img className="ncf-logo"src={imgUrl} alt="" />
-                <h1 className="logo-description">Nigeria Cricket Federation!</h1>
+                <h1 className="logo-description">Nigeria Cricket <br></br> Federation</h1>
+
+                <div className="cricket-nav">
+                <nav className="navbar">
+                    <a href="/home/"> Home </a> |
+                    <a href="/acusn/"> ACUSN </a> |
+                    <a href="/ls/"> Live Scores </a> |
+                    <a href="/about us/"> About us </a> |
+                    <a href="/contact us/"> Contact us </a> |
+                    <a href="/more/"> More </a> |
+                    <a href="/donate/"> Donate </a> |
+                    </nav>
+                </div>
             </div>
 
         </div>
